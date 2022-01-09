1d0b1051e51d600a1e608dbd0c0890d541b59c86
yakupbilgen <yakupbilgen92@gmail.com>
2022-01-09 15:55:02 +0300

    tahmidiyye constant

--- lib/screen/tahmidiyye.dart
+++ lib/screen/tahmidiyye.dart
@@ -8,29 +8,6 @@ class Tahmidiyye extends StatefulWidget {
 }
 
 final imageNumber = List.generate(22, (i) => i);
-final List wordList = [
-  "Allahu Ekber (10 defa)\nBismillahhirrahmanirrahim\nFerdun. Hayyun. Kayyumun. Hakemun. Adlun. Kuddusun.",
-  "1. Kusur ve noksanlıktan münezzeh olan Allah-u Tealaya (c.c.) iman, Onun birliği, isim ve sıfatlarından her biri için; ezelden ebede kadar isim ve sıfatlarının tecellilerinin tamamı adedince Alemlerin Rabbi olan Alllah'a hamd olsun. Ondan başka ilah yoktur.",
-  "2. Hz. Muhammed'in (a.s.m.) peygamberliği ve ona iman için; Hz. Muhammed Aleyhissalatü Vesselamın yaptığı iyilikler, sahip olduğu kemalat, getirdiği meyveler ve sağladığı faydalar adedince ve yine o Aleyhissalatü Vesselamın ali, Ashabı ve ümmetinin haseneleri sayısınca Alemlerin Rabbi olan Allah'a hamd olsun.",
-  "3. Kur'an ve ona iman için; Kur'an'ın zihinlerde tasavvur edilen manaları adedince ve onun Kıyamete kadar havada temessül eden kelime ve harfleri sayısınca Alemlerin Rabbi olan Allah'a hamd olsun.",
-  "4. İman, Kur'an ve İslam nimeti için; melekler, ruhaniler ve mahlukatın bütün zamanlarda yaptıkları tesbihler, getirdikleri hamd, tekbir ve tehliller sayısınca Alemlerin Rabbi olan Allah'a hamd olsun.",
-  "5. İman ve Kur'an nimeti için; yıldızlar ve onların hareketleri, duruşları ve zerreleri sayısınca Alemlerin Rabbi olan Allah'a hamd olsun.",
-  "6. İman ve Kur'an için; bitkiler, ağaçlar ve onların yaprakları, çiçekleri, meyveleri, tohumları, getirdikleri tesbih ve hamdleri sayısınca Alemlerin Rabbi olan Allah'a hamd olsun.",
-  "7. İman ve Kur'an nimeti için; kuşlar ve kuşcuklar, hayvanlar ve hayvancıklar, onların sesleri ve nefesleri, getirdikleri tesbihler ve hamdler sayısınca Alemlerin Rabbi olan Allah'a hamd olsun.",
-  "8. Allah'ın, yiyecekler ve nimetlerle bize olan ikramları için; taamlar, nimetler ve bunların çeşitleri, kokuları, tatları ve parçaları sayısınca Alemlerin Rabbi olan Allah'a hamd olsun.",
-  "9. Allah'ın iman, Kur'an ve Risale-i Nur; Kadir, Miraç, Berat, Regaib geceleriyle bize olan ikramından dolayı, Kadir, Miraç, Berat, Regaib gecelerinin aşireleri (Aşire; bir dakikanın 167 trilyon, 961 milyar 600 milyonda biridir.) ve yine Ramazan ayının aşireleri adedince ve Kıyamete kadar Risale-i Nur'un yazılan ve okunan harfleri sayısınca Alemlerin Rabbi olan Allah'a hamd olsun.",
-  "10. Kur'an nimeti için; inişinden Kıyamete kadar Kur'an'ın kelime ve harflerinin okunmasının sevapları sayısınca Alemlerin Rabbi olan Allah'a hamd olsun.",
-  "11. İman ve Allah'ın razı olduğu şeylerden ibaret olan İslamiyet nimeti için; Mü'min ve Müslümanların yaptıkları iyilikler sayısınca Alemlerin Rabbi olan Allah'a hamd olsun.",
-  "12. Kur'an, iman ve Risale- Nur nimeti için; kainatın zerreleri, terkibleri ve yaptıkları tesbihleri sayısınca Alemlerin Rabbi olan Allah'a hamd olsun.",
-  "13. Kur'an, iman ve İslamiyet için; bütün asır ve mekanlardaki ağaçların yaprakları, denizlerin dalgaları, yağmurların damlaları, kuşların nağmeleri, ışıkların parıltıları, çiçeklerin tohumları, meyvelerin çekirdekleri sayısınca Alemlerin Rabbi olan Allah'a hamd olsun.",
-  "14. İman ve Kur'an için; madenler ve varlıklar ile bunların hususiyetleri, verdikleri meyveler ve menfaatleri sayısınca Alemlerin Rabbi olan Allah'a hamd olsun.",
-  "15. İman, Kur'an ve İslam için; melekler, ruhlar, insanlar, cinler, bitkiler, hayvanlar ve bunların bütün zamanlarda yaptıkları hamdler, ettikleri tesbihler sayısınca Alemlerin Rabbi olan Allah'a hamd olsun.",
-  "16. Kur'an, iman ve Risale-i Nur nimeti için; denizleri, karaları, Arşı, Kürsiyi, Arzı ve Gökleri dolduracak kadar Alemlerin Rabbi olan Allah'a hamd olsun.",
-  "17. Ahiret ve ona iman için; Dünya ve Ahireti dolduracak kadar Alemlerin Rabbi olan Allah'a hamd olsun.",
-  "18. Cennet ve ona iman için; Cennetler dolusu ve sonsuza kadar Allah'ın onların ahalisine yapacağı ikramlar sayısınca Alemlerin Rabbi olan Allah'a hamd olsun.",
-  "19. Nimetlerinden her birisi için; nimetleri adedince ve Zatının layık olduğu şekilde Alemlerin Rabbi olan Allah'a hamd olsun.",
-  "20. Ey bütün kemal sıfatlarla muttasıf ve hakiki mabut olan Allah. Ey Dünyada mü'min-kafir ayırd etmeden herkesi rızıklandıran Rahman. Ey çok merhamet sahibi ve Ahirette sadece Mü'min kullarına bol merhamet edecek olan Rahim. Ey zat, sıfat ve ef'alinde ortağı bulunmayan Ferd. Ey gerçek hayat sahibi olan Hayy. Ey herşeyi ayakta tutan ve koruyan Kayyum. Ey hüküm ve kaza sahibi Hakem. Ey mutlak adalet sahibi Adl. Ey kusur ve eksiklikten beri olan ve herşeyi temizleyip arındıran Kuddus.",
-];
 
 class _TahmidiyyeState extends State<Tahmidiyye> {
   @override
