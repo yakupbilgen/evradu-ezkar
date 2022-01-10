14f51ff82ed3ad6999050e04f02c0ce456cd395f
yakupbilgen <yakupbilgen92@gmail.com>
2022-01-11 00:47:10 +0300

    add celcelutiye homepage

---
+++ lib/screen/celcelutiye/celcelutiye_homepage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CelcelutiyeHomepage extends StatefulWidget {
+  const CelcelutiyeHomepage({Key? key}) : super(key: key);
+
+  @override
+  _CelcelutiyeHomepageState createState() => _CelcelutiyeHomepageState();
+}
+
+class _CelcelutiyeHomepageState extends State<CelcelutiyeHomepage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Celcelutiye Homepage'),
+      ),
+    );
+  }
+}
