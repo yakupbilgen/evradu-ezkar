0387049e921cb01f390f396ffa7bf57589546242
yakupbilgen <yakupbilgen92@gmail.com>
2022-01-10 20:19:03 +0300

    new screen

---
+++ lib/screen/tahmidiyye/tahmidiyye_homepage.dart
@@ -0,0 +1,16 @@
+import 'package:evraduezkar/constant/tahmidiyye/tahmidiyye_constant.dart';
+import 'package:flutter/material.dart';
+
+class TahmidiyyeHomePage extends StatelessWidget {
+  const TahmidiyyeHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: TahmidiyyeConstant.bgColor,
+      body: Center(
+        child: Column(),
+      ),
+    );
+  }
+}
