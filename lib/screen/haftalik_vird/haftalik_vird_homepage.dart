6eb757e1c44ecb529f8ba9dac099a610c403d32e
yakupbilgen <yakupbilgen92@gmail.com>
2022-01-16 17:33:51 +0300

    add new screen

---
+++ lib/screen/haftalik_vird/haftalik_vird_homepage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HaftalikVirdHomepage extends StatefulWidget {
+  const HaftalikVirdHomepage({Key? key}) : super(key: key);
+
+  @override
+  _HaftalikVirdHomepageState createState() => _HaftalikVirdHomepageState();
+}
+
+class _HaftalikVirdHomepageState extends State<HaftalikVirdHomepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
